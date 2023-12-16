e0f8db459b5f7d367744d6afbe215ffe2d7ab535
TrongHB <tronghbgcc200034@fpt.edu.vn>
2023-12-16 14:15:35 +0700

    2

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
-$host="localhost";
-$username="root";
-$password="";
-$database="online_shopping";
+$host="u6354r3es4optspf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$username="qqxu6lhgtq2d2g9c";
+$password="o5vljl4ci7mk4kia";
+$database="eiqnngb1ak61y1v0";
 $conn= mysqli_connect($host,$username,$password,$database);
 mysqli_query($conn,"SET NAMES 'utf8'");
 if (mysqli_connect_error())
